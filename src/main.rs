cc6e81847fab1a20d98520f551f5bbca7a7b993e
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2022-06-03 23:37:55 +0200

    :construction: debug

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,16 @@
 use std::convert::Infallible;
 use std::net::SocketAddr;
-use hyper::{Body, Request, Response, Server, Method};
+use hyper::{Body, Request, Response, Server, Method, Error};
 use hyper::service::{make_service_fn, service_fn};
 
 use anyhow::Result;
 
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock, atomic::{Ordering, AtomicUsize}};
+
 
 use clap::Parser;
 
-async fn hello_world(req: Request<Body>) -> Result<Response<Body>> {
-
-    println!("{:?}", req);
-
-    let key = req.uri().path(); // TODO maybe remove "/"
-
-    let res = match req.method() {
-        &Method::GET => {
-            String::from("GET worked")
-        },
-        &Method::PUT => {
-            let val = hyper::body::to_bytes(req.into_body()).await?;
-            println!("based? {:?}", val);
-            String::from("PUT worked")
-        }
-        _ => String::from("bad"),
-    };
-    Ok(Response::new(res.into()))
-}
 
 const NAME: &str = "getput";
 const AUTHOR: &str = "github.com/{lquenti,meipp}";
@@ -50,28 +34,44 @@ struct Cli {
     port: u64,
 }
 
-/*
-fn main() {
-    let cli = Cli::parse();
-    println!("{:?}", cli);
+async fn handle_req(req: Request<&str>, counter: Arc<AtomicUsize>) {
+    println!("{:?}", req.method());
 }
-*/
 
 #[tokio::main]
 async fn main() {
-    // We'll bind to 127.0.0.1:3000
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = ([127, 0, 0, 1], 3000).into();
 
-    // A `Service` is needed for every connection, so this
-    // creates one from our `hello_world` function.
-    let make_svc = make_service_fn(|_conn| async {
-        // service_fn converts our function into a `Service`
-        Ok::<_, Infallible>(service_fn(hello_world))
+    let counter = Arc::new(AtomicUsize::new(0));
+
+    let make_service = make_service_fn(move |_| {
+        let counter = counter.clone();
+
+        async move {
+            Ok::<_, Error>(service_fn(move |req| {
+                handle_req(req, counter.clone())
+                /*
+                let key: &str = req.uri().path();
+                match req.method() {
+                    &Method::GET => {
+                        println!("GET {:?}", &key);
+                    }
+                    &Method::PUT => {
+                        println!("PUT {:?}", &key);
+                    }
+                    _ => {
+                        println!("anything else");
+                    },
+                }
+                */
+            }))
+        }
     });
 
-    let server = Server::bind(&addr).serve(make_svc);
+    let server = Server::bind(&addr).serve(make_service);
 
-    // Run this server for... forever!
+    println!("Listening on http://{}", addr);
+
     if let Err(e) = server.await {
         eprintln!("server error: {}", e);
     }
